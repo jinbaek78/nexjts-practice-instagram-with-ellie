f3d0670fa2bd53b911a354b52515a764342a5904
jinbaek <jinmb0602@gmail.com>
2023-08-25 21:44:20 +0900

    chore: set up sanity config

---
+++ sanity-studio/sanity.config.ts
@@ -0,0 +1,17 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Sanity Project',
+  projectId: process.env.SANITY_STUDIO_SANITY_PROJECT_ID!,
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
